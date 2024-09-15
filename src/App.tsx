0f4f8383d84fdf23fa41ee9e136168f7921d793a
Thomas_465 <160943727+Thomas465xd@users.noreply.github.com>
2024-09-15 19:13:25 -0300

    *Frontend for the home page -Created a BudgetContext component -Created a BudgetForm component -Created a custom hook, useBudget -Created the reducer, budget-reducer -Context defined using context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import BudgetForm from "./components/BudgetForm"
+
 function App() {
+
     return (
         <>
-            <h1>Personal Budget & Agenda</h1>
+            <header className="bg-orange-500 py-8 max-h-72">
+                <h1 className="uppercase text-center font-black text-4xl text-white">
+                    Personal Budget & Agenda
+                </h1>
+            </header>
+
+            <div className="max-w-3xl mx-auto bg-white shadow-lg roudned-lg mt-10 p-10">
+                <BudgetForm
+                
+                />
+            </div>
         </>
     )
 }
---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,50 @@
+import { ChangeEvent, FormEvent, useMemo, useState } from "react";
+import { useBudget } from "../hooks/useBudget";
+
+export default function BudgetForm() {
+    const [budget, setBudget] = useState<number | string>(0); // Allow string to handle empty input
+    const { dispatch } = useBudget()
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+
+        // Set the budget to an empty string if the input is empty, otherwise convert it to a number
+        setBudget(value === '' ? '' : Number(value));
+    };
+
+    const isValid = useMemo(() => {
+        return isNaN(Number(budget)) || Number(budget) <= 0;
+    }, [budget]);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        dispatch({type: "add-budget", payload: { budget: Number(budget)}})
+    }
+
+    return (
+        <form action="" className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-orange-500 font-bold text-center">
+                    Define Budget
+                </label>
+                <input
+                    id="budget"
+                    type="number"
+                    className="border border-orange-500 rounded px-3 py-2 text-orange-500 font-bold"
+                    placeholder="Enter your budget"
+                    name="budget"
+                    value={budget === 0 ? '' : budget} // Show empty string if budget is 0
+                    onChange={handleChange}
+                />
+            </div>
+
+            <input
+                type="submit"
+                value="Define budget"
+                className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-full uppercase disabled:opacity-40"
+                disabled={isValid}
+            />
+        </form>
+    );
+}
